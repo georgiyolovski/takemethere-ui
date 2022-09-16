4a03899684778fbd575c8fa2ab7ce771d17d4231
Mayya Markova <76959453+mayyamark@users.noreply.github.com>
2022-09-16 20:18:16 +0300

    UI Setup (#1)
    * Initial setup
    
    * Add ESLint config
    
    * Add MUI5
    
    * Add PWA setup
    
    * Add png icons and fix PWA setup

---
+++ pages/_app.tsx
@@ -0,0 +1,33 @@
+import { CacheProvider, EmotionCache } from '@emotion/react';
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+import createEmotionCache from '../src/theme/createEmotionCache';
+import theme from '../src/theme/theme';
+
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+
+interface IMyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
+}
+
+const MyApp = (props: IMyAppProps) => {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <title>Take Me There</title>
+        <meta name='viewport' content='initial-scale=1, width=device-width' />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
+};
+
+export default MyApp;
---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+import Typography from '@mui/material/Typography';
+import type { NextPage } from 'next';
+
+const Home: NextPage = () => {
+  return <Typography>Hello world</Typography>;
+};
+
+export default Home;
