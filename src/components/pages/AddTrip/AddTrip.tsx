848537c0179bb8dafaabc079b136dd77d84f1958
Mayya Markova <mayya.markova@gmail.com>
2022-09-17 19:48:00 +0300

    Add search destination

--- src/components/pages/AddTrip/AddTrip.tsx
+++ src/components/pages/AddTrip/AddTrip.tsx
@@ -6,46 +6,55 @@ import FormGroup from '@mui/material/FormGroup';
 import Grid from '@mui/material/Grid/Grid';
 import TextField from '@mui/material/TextField/TextField';
 import Box from '@mui/system/Box';
+import format from 'date-fns/format';
 import { useState } from 'react';
+import { apiEndpoint } from '../../../constants';
+import { useAuth } from '../../../context/AuthContext';
 import Layout from '../../big/Layout/Layout';
 import DatePicker from '../../small/DatePicker/DatePicker';
+import DestinationAutocomplete from '../../small/DestinationAutocomplete/DestinationAutocomplete';
 import { Form } from '../Register/Register.styled';
 
 const AddTrip = () => {
+  const { auth } = useAuth();
+
   const [people, setPeople] = useState({
     children: 0,
     adults: 0,
   });
   const [dateRange, setDateRange] = useState<{
-    from: Date | null;
-    to: Date | null;
+    from: string | null;
+    to: string | null;
   }>({ from: null, to: null });
   const [activities, setActivities] = useState<string[]>([]);
+  const [location, setLocation] = useState<null | number>(null);
 
+  const [searchSessionId, setSearchSessionId] = useState<null | number>(null);
   const handleSubmit = async (ev: React.FormEvent<HTMLFormElement>) => {
     ev.preventDefault();
-    // const data = new FormData(ev.currentTarget);
 
-    // const response = await fetch(`${apiEndpoint}/auth/register`, {
-    //   method: 'POST',
-    //   headers: {
-    //     Accept: 'application/json',
-    //     'Content-Type': 'application/json',
-    //   },
-    //   body: JSON.stringify({
-    //     email: data.get('email'),
-    //     first_name: data.get('firstName'),
-    //     last_name: data.get('lastName'),
-    //     password: data.get('password'),
-    //   }),
-    // });
+    const response = await fetch(`${apiEndpoint}/search_sessions`, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        Authorization: `${auth?.token}`,
+      },
+      body: JSON.stringify({
+        location_id: location,
+        activities,
+        start_date: dateRange.from,
+        end_date: dateRange.to,
+        ...people,
+      }),
+    });
 
-    // const json = await response.json();
+    const json = await response.json();
 
-    // if (json.token) {
-    // }
+    if (json.id) {
+      setSearchSessionId(json.id);
+    }
   };
-  console.log(activities);
 
   const handleCheckboxChange = (ev: React.ChangeEvent<HTMLInputElement>) => {
     if (ev.target.checked) {
@@ -70,12 +79,8 @@ const AddTrip = () => {
             </Box>
           </Grid>
           <Grid item xs={12}>
-            <TextField
-              required
-              fullWidth
-              id='destination'
-              label='Destination'
-              name='destination'
+            <DestinationAutocomplete
+              onChange={(id: number | null) => setLocation(id)}
             />
           </Grid>
 
@@ -84,9 +89,14 @@ const AddTrip = () => {
               label='From'
               required
               value={dateRange.from}
-              onChange={(newDate) =>
-                setDateRange((prevState) => ({ ...prevState, from: newDate }))
-              }
+              onChange={(newDate) => {
+                if (newDate) {
+                  setDateRange((prevState) => ({
+                    ...prevState,
+                    from: format(newDate, 'yyyy-LL-dd'),
+                  }));
+                }
+              }}
             />
           </Grid>
 
@@ -95,9 +105,14 @@ const AddTrip = () => {
               label='To'
               required
               value={dateRange.to}
-              onChange={(newDate) =>
-                setDateRange((prevState) => ({ ...prevState, to: newDate }))
-              }
+              onChange={(newDate) => {
+                if (newDate) {
+                  setDateRange((prevState) => ({
+                    ...prevState,
+                    to: format(newDate, 'yyyy-LL-dd'),
+                  }));
+                }
+              }}
             />
           </Grid>
 
---
+++ src/components/small/DestinationAutocomplete/DestinationAutocomplete.tsx
@@ -0,0 +1,122 @@
+import SearchIcon from '@mui/icons-material/Search';
+import MuiAutocomplete from '@mui/material/Autocomplete';
+import CircularProgress from '@mui/material/CircularProgress/CircularProgress';
+import InputAdornment from '@mui/material/InputAdornment/InputAdornment';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+import { debounce } from 'lodash';
+import { useState } from 'react';
+import { apiEndpoint } from '../../../constants';
+import { useAuth } from '../../../context/AuthContext';
+
+interface IOption {
+  country: string;
+  id: number;
+  name: string;
+}
+
+const DestinationAutocomplete = ({
+  onChange,
+}: {
+  onChange: (id: number | null) => void;
+}) => {
+  const { auth } = useAuth();
+
+  const [options, setOptions] = useState<IOption[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [inputValue, setInputValue] = useState('');
+
+  const performSearch = (value: string) => {
+    setLoading(true);
+
+    fetch(`${apiEndpoint}/locations?name=${value}`, {
+      headers: {
+        Authorization: `${auth?.token}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setOptions(data);
+      })
+      .catch(() => {
+        setOptions([]);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  return (
+    <MuiAutocomplete
+      fullWidth
+      openOnFocus
+      freeSolo
+      clearOnEscape
+      options={options}
+      inputValue={inputValue}
+      onInputChange={(_, value, reason) => {
+        setInputValue(value);
+        if (reason === 'input' && value.length > 3) {
+          setOptions([]);
+          onChange(null);
+
+          debounce(performSearch, 300)(value);
+        } else if (value.length === 0) {
+          setOptions([]);
+          onChange(null);
+        }
+      }}
+      onChange={(_, option) => {
+        if (typeof option === 'string' || !option?.id) {
+          onChange(null);
+        } else {
+          onChange(option.id);
+        }
+      }}
+      loading={loading}
+      renderOption={(props, option) => (
+        <Typography
+          variant='body2'
+          key={`${option.name}-${option.country}`}
+          {...props}
+        >
+          {option.name}, {option.country}
+        </Typography>
+      )}
+      getOptionLabel={(option) => {
+        if (typeof option === 'string') {
+          return '';
+        } else {
+          return `${option.name}, ${option.country}`;
+        }
+      }}
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          fullWidth
+          label='Destination'
+          variant='outlined'
+          InputProps={{
+            ...params.InputProps,
+            startAdornment: (
+              <InputAdornment position='start'>
+                <SearchIcon />
+              </InputAdornment>
+            ),
+            endAdornment: loading ? (
+              <CircularProgress color='primary' size={20} />
+            ) : (
+              params.InputProps.endAdornment
+            ),
+          }}
+          inputProps={{
+            ...params.inputProps,
+            autoComplete: 'no',
+          }}
+        />
+      )}
+      disabled={loading}
+    />
+  );
+};
+export default DestinationAutocomplete;
