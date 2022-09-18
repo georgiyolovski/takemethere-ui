7acbfdb30934dc0db9b4286a431429b3de28a5b7
Georgi Yolovski <georgi.yolovski@gannett.com>
2022-09-18 13:36:24 +0300

    Fixed Google login (#11)
    * Fixed Google login
    
    * Rebased

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@mui/material/styles';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 import { AuthProvider } from '../src/context/AuthContext';
 import createEmotionCache from '../src/theme/createEmotionCache';
 import theme from '../src/theme/theme';
@@ -18,18 +19,24 @@ const MyApp = (props: IMyAppProps) => {
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
 
   return (
-    <CacheProvider value={emotionCache}>
-      <Head>
-        <title>Take Me There</title>
-        <meta name='viewport' content='initial-scale=1, width=device-width' />
-      </Head>
-      <ThemeProvider theme={theme}>
-        <AuthProvider>
-          <CssBaseline />
-          <Component {...pageProps} />
-        </AuthProvider>
-      </ThemeProvider>
-    </CacheProvider>
+    <GoogleOAuthProvider
+      clientId={
+        '27497645030-8m09bgm6vpleibfqdg06u4bq8aid4n6e.apps.googleusercontent.com'
+      }
+    >
+      <CacheProvider value={emotionCache}>
+        <Head>
+          <title>Take Me There</title>
+          <meta name='viewport' content='initial-scale=1, width=device-width' />
+        </Head>
+        <ThemeProvider theme={theme}>
+          <AuthProvider>
+            <CssBaseline />
+            <Component {...pageProps} />
+          </AuthProvider>
+        </ThemeProvider>
+      </CacheProvider>
+    </GoogleOAuthProvider>
   );
 };
 
